9b9ea1be9439c92a6d7ac982db74b61143845817
Jahongir <jahongir22@gmail.com>
2022-05-10 17:00:17 +0500

    dev

---
+++ script.js
@@ -0,0 +1,9 @@
+let zina = +prompt('zinalar sonini kiriting')
+let qator = prompt('qator sonini kiriting')
+let belgi = prompt('belgi sonini kiriting')
+
+
+console.info(belgi)
+for(let i = 1; i < zina; i++){
+console.info(belgi = qator + belgi)
+}
